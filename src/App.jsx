f95c9fe5c30accfa4bff5408103fadbc6d0a3d1c
Anusri Karmokar <anusrikarmokar@Anusris-MacBook-Air.local>
2025-04-19 19:50:26 +0530

    updated home path in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import NotFound from "./components/NotFound";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
